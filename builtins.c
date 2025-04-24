9af0f64fa23e54e63e73a21d8f761c7b74304c98
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-04-24 17:27:24 +0200

    add of built-in env

--- builtins.c
+++ builtins.c
@@ -1,12 +1,14 @@
 #include "shell.h"
 
 /**
- * handle_builtin - Gère les commandes internes comme "exit"
+ * handle_builtin - Gère les commandes internes comme "exit" et "env"
  * @argv: tableau des arguments
  * Return: 1 si un builtin a été exécuté, 0 sinon
  */
 int handle_builtin(char **argv)
 {
+	int count;
+
 	if (!argv || !argv[0])
 		return (0);
 
@@ -14,6 +16,15 @@ int handle_builtin(char **argv)
 	{
 		free(argv);
 		exit(0);
+	}
+
+	if (strcmp(argv[0], "env") == 0)
+	{
+		for (count = 0; environ[count]; count++)
+			printf("%s\n", environ[count]);
+
+		free(argv);
+		return (1);
 	}
 
 	return (0);

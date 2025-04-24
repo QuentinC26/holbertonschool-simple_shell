3f3b701db3a09a302c7309e1f8b3fd0ad4f79561
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-04-24 17:20:32 +0200

    add of prototype

--- shell.h
+++ shell.h
@@ -16,6 +16,7 @@ char *read_line(void);
 void execute_cmd(char *line);
 char *_strdup(const char *str);
 char **tokenize_line(char *line);
+char *my_getenv(const char *name);
 int handle_builtin(char **argv);
 char *find_command_path(char *command);
 
